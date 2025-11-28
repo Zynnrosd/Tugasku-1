7c994d4a57581f8ae683c7dcffac64b4992a76ba
Zynnrosd <naufan.r668@gmail.com>
2025-11-28 16:20:21 +0700

    test

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,3 +20,5 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
+export default app;
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://10.131.139.95:5000/api",
+  baseURL: "http://192.168.1.4:5000/api",
 });
 
 export default api;
